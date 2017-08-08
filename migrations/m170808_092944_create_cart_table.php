d27ebf80ce67dfe8a65b832f9051d06ea8ae7a05
Prosvetov Evgeniy <prosvetov.work95@yandex.ru>
2017-08-08 16:59:54 +0700

    add cart and cart_items tables

---
+++ migrations/m170808_092944_create_cart_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `cart`.
+ */
+class m170808_092944_create_cart_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+   public function up()
+    {
+        $this->createTable('cart', [
+            'id' => $this->string('36')->notNull(),
+            'customer_id' => $this->integer(),
+        ]);
+
+        $this->addForeignKey('cart_customer_id', 'cart', 'customer_id','customer','id');
+    }
+
+
+    public function down()
+    {
+        $this->dropForeignKey('cart_customer_id', 'cart');
+        $this->dropTable('cart');
+    }
+}
---
+++ migrations/m170808_094020_create_cart_items_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `cart_items`.
+ */
+class m170808_094020_create_cart_items_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('cart_items', [
+            'cart_id' => $this->string('36')->notNull(),
+            'product_id' => $this->integer()->notNull(),
+            'amount' => $this->integer()->notNull()->defaultValue(1),
+        ]);
+        $this->addForeignKey('cart_items_product_id', 'cart_items', 'product_id', 'product', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('cart_items_product_id','cart_items');
+        $this->dropTable('cart_items');
+    }
+}
