690105fb3244cdac5ce9d239a5d13e0abc3adaa6
Prosvetov Evgeniy <prosvetov.work95@yandex.ru>
2017-09-15 11:57:30 +0700

    initialization environment

---
+++ tests/unit/logic/CartItemsTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace logic;
+
+
+class CartItemsTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
---
+++ tests/unit/logic/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace logic;
+
+class UserTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
